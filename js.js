e757023aa96020d82094459219d743c76c01e82a
Pavel <yartsev.pavel@gmail.com>
2020-04-15 00:08:31 +0300

    Added case for Switch-case operator

--- js.js
+++ js.js
@@ -1,6 +1,30 @@
-if (2*4 == 8) {
-    console.log("Верно");
-} else {
-    console.log("Неверно");
-}
+let num = 50;
 
+if (num < 49){
+    console.log('Неверно!');
+} else if (num > 100) {
+    console.log('Много!');
+} else {
+    console.log('Верно!');
+};
+
+(num == 50) ? console.log('Верно!') : console.log('Неверно!');
+
+switch(num) {
+    case num < 49:
+        console.log('Неверно!');
+        break;
+    case num > 100:
+        console.log('Много!');            
+        break;
+    case num > 100:
+        console.log('Все еще много!');            
+        break;
+    case 50:  //Запомнить, что case num == 50 неверный формат условия!
+        console.log('Верно!');            
+        break;    
+    default:
+        console.log("Что-то пошло не так!");
+        break;
+
+}
